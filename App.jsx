f76e6facbb98f2d37441bec4f0e2ad76a3a8b962
Kra1m <109236390+Kra1m@users.noreply.github.com>
2023-04-29 00:14:39 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import './App.css'
+import Header from './assets/components/Header/Header'
+import Homepage from "./assets/components/Homepage"
+
+function App() {
+  return(
+  <React.Fragment>
+    <Header />
+    <Homepage />
+    </React.Fragment>
+  )
+}
+
+export default App
